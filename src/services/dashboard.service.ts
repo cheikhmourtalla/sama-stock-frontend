4b71c74efc755ad5e5ddc336a85a1ab5d91c6fd6
mouhamed2213 <Mouhamedbk1002@gmail.com>
2026-05-12 22:23:27 +0000

    refactor api reponse

--- src/services/dashboard.service.ts
+++ src/services/dashboard.service.ts
@@ -3,5 +3,5 @@ import type { DashboardStats } from "../types/dashboard";
 
 export const getDashboardStats = async (): Promise<DashboardStats> => {
   const response = await api.get("/dashboard/stats");
-  return response.data;
+  return response.data.data;
 };
